3c3ac765f975456df0a7f18d18cad0a9560c6923
Ilya Chumakov <11814549+ilya-chumakov@users.noreply.github.com>
2025-08-09 12:18:16 +0200

    Use intended writer in registry emitter

---
+++ src/Skybeam/IndentedTextWriterExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.CodeDom.Compiler;
+
+namespace Skybeam;
+
+public static class IndentedTextWriterExtensions
+{
+    public static void OpenBlock(this IndentedTextWriter writer)
+    {
+        writer.WriteLine("{");
+        writer.Indent++;
+    }
+
+    public static void CloseBlock(this IndentedTextWriter writer)
+    {
+        writer.Indent--;
+        writer.WriteLine("}");
+    }
+}
